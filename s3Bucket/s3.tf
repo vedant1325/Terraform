a5f8e08da29257f3857eb9576c13f39ce5ce8727
Vedant Dange <vedantdange149@gmail.com>
2025-09-08 22:41:04 +0530

    Initial commit of terraform s#

---
+++ s3Bucket/s3.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+  profile = "configs"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-123456"
+  
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
