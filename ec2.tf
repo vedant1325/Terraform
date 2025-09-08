e381955cbfb9569bfbb4f2ad4bc418a76e15fc7e
Vedant Dange <vedantdange149@gmail.com>
2025-09-08 21:09:43 +0530

    Initial commit of terraform ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+    region="ap-south-1"
+    profile="configs"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0c4a668b99e68bbde"
+    instance_type = "t2.micro"
+    key_name = "serverkp"
+    vpc_security_group_ids = ["sg-01e29a8691de24a5f"]
+     tags = {
+    Name = "MyExampleInstance"
+  }
+  
+}
